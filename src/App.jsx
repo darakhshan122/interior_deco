81ca34e18b49e04229b37daf46c97c330157a497
darakhshan122 <physicswallahmodule872@gmail.com>
2024-10-16 15:06:15 +0530

    Switched to HashRouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 
+// // src/App.jsx
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './components/home';
+// import Contact from './components/contact';
+// import Gallery from './components/gallery';
+// import Navbar from './components/navbar';
+// import Footer from './components/footer';
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="flex flex-col min-h-screen">
+//         {/* Navbar is rendered on all pages */}
+//         <Navbar />
+//         <div className="flex-grow">
+//           {/* Routes for different pages */}
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/gallery" element={<Gallery />} />
+//           </Routes>
+//         </div>
+//         {/* Footer is rendered on all pages */}
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Change here
 import Home from './components/home';
 import Contact from './components/contact';
 import Gallery from './components/gallery';
@@ -30,3 +61,4 @@ function App() {
 }
 
 export default App;
+
